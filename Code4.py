7b4258c780b6034dcb1571ed2691eeeed05e1ea9
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2019-04-27 12:00:49 -0500

    Add files via upload

--- Code4.py
+++ Code4.py
@@ -1,23 +1,12 @@
 room=[
-[1,1,1,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,0,0,1],
-[1,1,1,1]
+[1,1,0,1,1],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,0,0,0,0],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,1,1,1,1],
 ]
-"""
-for j in range (len(room)):
-    Gideon=""
-    for i in range (len(room[j])):
-        Gideon=Gideon+str(room[j][i])
-    print (Gideon)"""
     
 WIDTH=800
 LENGTH=600
@@ -27,8 +16,8 @@ toplefty=150
 
 OBJECT_LIST=[images.floor, images.pillar]
 
-roomheight=12
-roomwidth=4
+roomheight=7
+roomwidth=5
 
 def draw():    
     for y in range (roomheight):
@@ -37,5 +26,3 @@ def draw():
             drawing=OBJECT_LIST[item]
             
             screen.blit(drawing, (topleftx+x*30, toplefty+y*30-drawing.get_height()))
-            
-            
--- Code5.py
+++ Code5.py
@@ -6,9 +6,20 @@ def comproom(height, width, exittop, exitright):
     room.append([1]*width)
     
     for i in range (height-2):
-        room.append(for j in range (width-2):
-            fakerow.append(0))
+        for j in range (width-2):
+            fakerow.append(0)
+        fakerow.append(1)
+        room.append(fakerow)
+        fakerow=[1]
     
-    print(room)
+    room.append([1]*width)
     
-comproom(7,5,3,4)
+    if exittop:
+        room[0][round(int(width/2),0)]=0
+    if exitright:
+        room[round(int(height/2),0)][width-1]=0
+    
+    return(room)
+    
+x=comproom(7,5,True,True)
+print(x)
